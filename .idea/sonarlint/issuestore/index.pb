6b8ace80bdd7f50bf40bea17d9402cb4fdba3fee
maria.abas <maria.abas@est.fib.upc.edu>
2020-06-07 22:15:30 +0200

    Canvis bot

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,5 +3,11 @@ A
 package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
 <
 package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
-F
-src/main/learn/main.js,0\1\01b1bd442adabed6aab3aff29c19eaf40b146c1c
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+W
+'src/main/repository/RedditRepository.js,c\3\c3776679e0b15f4a4118208895209652d35bc25c
+@
+src/main/main.js,3\3\33e3f8a9fc6ff2e461a2aa32a384cf43a6ca1f59
+=
+learn-bot.iml,e\4\e468a4f9fb9f5110b62f7e3e2e58391c4042e84f
--- src/main/main.js
+++ src/main/main.js
@@ -1,21 +1,34 @@
-const Telegraf = require('telegraf')
-/*const RedditRepository = require('./repository')*/
+const Telegraf = require("telegraf")
+const axios = require('axios')
 const TOKEN_TELEGRAM = "1120886143:AAFO0hBoN65703lGn1InTlbFC_efofaNsy4"
 
 const bot = new Telegraf(TOKEN_TELEGRAM)
-/*const reddit = new RedditRepository('/RedditRepository');*/
 
 bot.hears('hi', ctx => {
     return ctx.reply('Hey!');
 })
+
 bot.start((ctx) => ctx.reply('Welcome!'))
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
+
 bot.on( 'sticker', (ctx) => ctx.reply('👍'))
-/*bot.on( 'text', (ctx => {
-        var message = ctx.message.text
-        var responseReddit = reddit.getInformationFromReddit(message)
-        return ctx.reply(responseReddit);
+
+bot.on( 'text', (ctx => {
+    const message = ctx.message.text;
+    axios
+        .get(`https://reddit.com/r/${message}/top.json?limit=10`)
+        .then(response => {
+            const data = response.data.data;
+            if (data.children.length < 1) {
+                return ctx.reply("The message couldn't be found");
+            }
+            const link = `https://reddit.com/${data.children[0].data.permalink}`;
+            return ctx.reply(link);
+        })
+        .catch(error => console.log(error));
     })
-)*/
+)
 
 bot.startPolling();
+
